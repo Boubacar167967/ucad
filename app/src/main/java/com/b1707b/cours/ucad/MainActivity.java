d5ad213e1d05eb932215cf76ae39572c4279ba1d
Arona221 <112222335+Arona221@users.noreply.github.com>
2022-12-08 16:15:58 +0000

    nouveau

--- app/src/main/java/com/b1707b/cours/ucad/MainActivity.java
+++ app/src/main/java/com/b1707b/cours/ucad/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "oauis ca marche", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "salutca marche", Toast.LENGTH_SHORT).show();
     }
 }
