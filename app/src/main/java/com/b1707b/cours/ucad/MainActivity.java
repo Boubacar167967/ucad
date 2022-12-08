71a48e2821e1ef748739bec50791ed06451a08d0
boubacar167967 <boubacarcidi77@gmail.com>
2022-12-08 16:19:23 +0000

    ajout encors

--- app/src/main/java/com/b1707b/cours/ucad/MainActivity.java
+++ app/src/main/java/com/b1707b/cours/ucad/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "oauis ca marche", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "salutca marche", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "oauis ca va alhamdoulilah", Toast.LENGTH_SHORT).show();
     }
 }
